8a6d407219122946aa13c74839da50cdfe19d671
krutik2377 <krutikgevariya7723@gmail.com>
2024-03-19 17:03:24 -0400

    Backend Setup

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+//write basic express boilerlater ConvolverNode,
+// with express.json().middleware
+
+const express = require("express");
+const app  = express();
+
+app.use(express.json());
+
+app.post("", function(req,res) {
+
+})
+
+
+app.get("", function(req,res) {
+    
+})
+
+app.put("", function(req,res) {
+    
+})
